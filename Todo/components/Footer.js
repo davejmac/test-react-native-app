a98e5fe96693397f72f5929063b059dea5ffdf92
davejmac <david.mcnamara@outlook.com>
2016-08-11 15:37:35 -0400

    wrap footer bare text in Text components

--- Todo/components/Footer.js
+++ Todo/components/Footer.js
@@ -1,22 +1,26 @@
 import React from 'react'
+import { View, Text } from 'react-native';
 import FilterLink from '../containers/FilterLink'
 
+
 const Footer = () => (
-  <p>
-    Show:
-    {" "}
-    <FilterLink filter="SHOW_ALL">
-      All
-    </FilterLink>
-    {", "}
-    <FilterLink filter="SHOW_ACTIVE">
-      Active
-    </FilterLink>
-    {", "}
-    <FilterLink filter="SHOW_COMPLETED">
-      Completed
-    </FilterLink>
-  </p>
+  <View>
+    <Text>
+      Show:
+      {" "}
+      <FilterLink filter="SHOW_ALL">
+        All
+      </FilterLink>
+      {", "}
+      <FilterLink filter="SHOW_ACTIVE">
+        Active
+      </FilterLink>
+      {", "}
+      <FilterLink filter="SHOW_COMPLETED">
+        Completed
+      </FilterLink>
+    </Text>
+  </View>
 )
 
 export default Footer;
