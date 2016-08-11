a96ab273ff1ae9417212fd585441d9addc80810f
davejmac <david.mcnamara@outlook.com>
2016-08-11 15:04:53 -0400

    fix some paths that broke from moving the todo app into a subdir

--- Todo/components/Index.js
+++ Todo/components/Index.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
+import todoApp from '../reducers'
+import App from './App'
 
 let store = createStore(todoApp)
-
 
 const Index = () => (
   <Provider store={store}>
