69d193c5ef702b0a5f2012d111c64fa3656d8990
davejmac <david.mcnamara@outlook.com>
2016-08-11 15:02:54 -0400

    import Index component for registering

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 'use strict';
 
 import React, {AppRegistry} from 'react-native';
-import App from './Todo/components/App';
+import Index from './Todo/components/App';
 
-AppRegistry.registerComponent('TDN', () => App);
+AppRegistry.registerComponent('TDN', () => Index);
